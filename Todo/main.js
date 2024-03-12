2787f9567a00ab14b0e98cc39ea7ee4ccb0419e7
KimMinJin63 <rlaalswls9663@gmail.com>
2024-03-12 15:45:54 +0900

    Todo 완성 및 html, css 연습

--- Todo/main.js
+++ Todo/main.js
@@ -1,36 +1,148 @@
-// 유저가 값을 입력한다.
-// +버튼을 클릭하면 할일이 추가된다.
-// delete 버튼을 누르면 할일이 삭제된다.
-// check 버튼을 누르면 할일이 끝나면서 밑주링 간다.
-// Doing, Done 탭을 누르면 언더바가 이동한다.
-// Done 탭은 Done 상태인 아이템만 Doing 탭은 Doing 상태인 아이템만 뜨게 한다.
-// All 탭을 누르면 다시 전체 아이템으로 돌아온다.
+let taskInput = document.getElementById("task-input");
+let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs a");
+let underline = document.getElementById("under-line");
+let menu = document.querySelectorAll("nav:first-child a");
+let mode = "all";
+let taskList = [];
+let filterList = [];
 
-let taskInput = document.getElementById('task-input');
-let addButton = document.getElementById('add-button')
-let taskList = []
+console.log(addButton);
+console.log(tabs);
 
-addButton.addEventListener('click', addTask);
+addButton.addEventListener("click", addTask);
 
 function addTask() {
-    let taskContent = taskInput.value
-    taskList.push(taskContent)
-    console.log(taskList)
-    render()
+  let task = {
+    id: randomId(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  render();
+  console.log(taskList);
 }
 
-
 function render() {
-    let resultHTML = ''
-    for (let i = 0; i < taskList.length; i++) {
-        resultHTML += `  <div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-            <button>Check</button>
-            <button>Delete</button>
-        </div>
-    </div>`;
-        
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "doing" || mode === "done") {
+    list = filterList;
+  }
+  let resultHTML = "";
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
+      resultHTML += `<div class="task">
+      <div class = "task-done">
+          ${list[i].taskContent}
+      </div>
+      <div>
+          <button onclick = "toggleComplete('${list[i].id}')">
+              Check
+          </button>
+          <button>
+              Delete
+          </button>
+      </div>
+  </div>`;
+    } else {
+      resultHTML += `<div class="task">
+      <div>
+          ${list[i].taskContent}
+      </div>
+      <div>
+          <button onclick = "toggleComplete('${list[i].id}')">
+              Check
+          </button>
+          <button onclick = "toggleDelete('${list[i].id}')">
+              Delete
+          </button>
+      </div>
+  </div>`;
     }
-    document.getElementById('task-board').innerHTML = resultHTML
+  }
+  document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  console.log("id :", id);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  console.log(taskList);
+}
+
+function randomId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
+
+function toggleDelete(id) {
+  console.log(taskList);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  if (mode === "all") {
+    render();
+  } else if (mode === "doing" || mode === "done") {
+    filter({ target: { id: mode } });
+  }
+  console.log(taskList);
+}
+
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
+
+function filter(event) {
+  console.log("filter", event.target.id);
+  mode = event.target.id;
+  if (mode === "all") {
+    render();
+  } else if (mode === "doing") {
+    filterList = [];
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+    console.log(filterList);
+  } else if (mode === "done") {
+    filterList = [];
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+}
+
+menu.forEach((menu) => menu.addEventListener("click", (e) => menuIndicator(e)));
+
+function menuIndicator(e) {
+  underline.style.left = e.currentTarget.offsetLeft + "px";
+  underline.style.width = e.currentTarget.offsetWidth + "px";
+  underline.style.top =
+    e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
+
+function enterKeyPressed(event) {
+  if (event.keyCode == 13) {
+    addTask();
+    taskInput.value = "";
+    return true;
+  } else {
+    return false;
+  }
 }
